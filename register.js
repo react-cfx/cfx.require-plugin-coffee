aaf30c90a94b8dedef7973f811d08526bad65e24
FooTearth <footearth@gmail.com>
2017-11-29 12:36:29 +0800

    remove dist file and add to ignore list && update to 0.0.3

--- register.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var coffee;
-coffee = require('./');
-module.exports = function (_require) {
-    var register;
-    ({register} = _require([coffee()]));
-    return register();
-};
